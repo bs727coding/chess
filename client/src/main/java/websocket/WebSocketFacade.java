6bd7a81bdc58eb43fa9a7304b2ed661b45e79a78
bs727coding <blakeman.sanders@gmail.com>
2025-03-25 15:19:52 -0600

    Reorganized file structure

--- client/src/main/java/websocket/WebSocketFacade.java
+++ client/src/main/java/websocket/WebSocketFacade.java
@@ -1,4 +1,43 @@
 package websocket;
 
-public class WebSocketFacade {
+import com.google.gson.Gson;
+import exception.ResponseException;
+import websocketmessages.Notification;
+
+import javax.websocket.*;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URI;
+
+public class WebSocketFacade extends Endpoint {
+
+    Session session;
+    NotificationHandler notificationHandler;
+
+    public WebSocketFacade(String url, NotificationHandler notificationHandler) throws ResponseException {
+        try {
+            url = url.replace("http", "ws");
+            URI socketURI = new URI(url + "/ws");
+            this.notificationHandler = notificationHandler;
+
+            WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+            this.session = container.connectToServer(this, socketURI);
+
+            this.session.addMessageHandler(new MessageHandler.Whole<String>() {
+                @Override
+                public void onMessage(String message) {
+                    Notification notification = new Gson().fromJson(message, Notification.class);
+                    notificationHandler.notify(System.out, notification); //pass in printStream higher up?
+                }
+            });
+        } catch (DeploymentException | IOException | URISyntaxException ex) {
+            throw new ResponseException(500, ex.getMessage());
+        }
+    }
+
+    @Override
+    public void onOpen(Session session, EndpointConfig endpointConfig) {
+    }
+
+    public void connect()
 }
